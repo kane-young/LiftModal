5cc654d07bfe142be928be04ecbd3dfd9fed40c3
kane-young <lyw2100@naver.com>
2025-05-03 17:56:55 +0900

    [feat] Package 세팅 및 SampleApp 설정

---
+++ LiftModal/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LiftModal",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "LiftModal",
+            targets: ["LiftModal"]),
+    ],
+    targets: [
+        .target(
+            name: "LiftModal", path: "Sources/LiftModal"),
+        .testTarget(
+            name: "LiftModalTests",
+            dependencies: ["LiftModal"]),
+    ]
+)
---
+++ LiftModalSample/LiftModalSample/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  LiftModalSample
+//
+//  Created by 이영우 on 5/3/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
